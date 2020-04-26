49aee5ade61946d84670825c6d0def4c819ce50e
jimenaRL <jimena@melix.net>
2020-04-26 17:37:37 +0200

    PEP8

--- python/audiovocana/ffmpeg_utils/ffprobe_utils.py
+++ python/audiovocana/ffmpeg_utils/ffprobe_utils.py
@@ -1,9 +1,9 @@
 import subprocess
-from dzr_utils.conf import FFPROBE_BINARY
+from audiovocana.conf import FFPROBE_BINARY
 
 import json
 
-possible_entries =  {
+possible_entries = {
                         "codec_name",
                         "codec_type",
                         "sample_fmt",
@@ -22,20 +22,34 @@ def get_ffprobe_entry(audio_filename, entry, entry_type="stream"):
         Get entry with ffprobe
     """
     if entry not in possible_entries:
-        raise ValueError("%s is not in the set of possible entries. Must be in: %s"%(entry, str(possible_entries)))
-    status,output = subprocess.getstatusoutput(" ".join([FFPROBE_BINARY, "-v error", "-show_entries","%s=%s"%(entry_type,entry),"-of default=noprint_wrappers=1:nokey=1",audio_filename]))
+        e = f"{entry} is not in the set of possible entries. "
+        e += f"Must be in: {possible_entries}."
+        raise ValueError(e)
+    cmd1 = " ".join([
+        FFPROBE_BINARY,
+        "-v error",
+        "-show_entries",
+        f"{entry_type,}={entry}",
+        "-of default=noprint_wrappers=1:nokey=1",
+        audio_filename])
+    status, output = subprocess.getstatusoutput(cmd1)
     if status:
-        raise OSError("ffprobe command returns with status %d. Output: %s"%(status, output))
+        e = "ffprobe command returns with status {status}. Output: {output}."
+        raise OSError(e)
     return output
+
 
 def get_infos(audio_filename):
     """
         Get info about an audio file with ffprobe
     """
     audio_filename = "'"+audio_filename+"'"
-    status,output = subprocess.getstatusoutput("ffprobe -v quiet -print_format json -show_format -show_streams %s"%audio_filename)
+    cmd = f"ffprobe -v quiet -print_format json -show_format -show_streams "
+    cmd += f"{audio_filename}"
+    status, output = subprocess.getstatusoutput(cmd)
     if status:
-        raise OSError("ffprobe command returns with status %d. Output: %s"%(status, output))
+        raise OSError(
+            f"ffprobe command returns with status {status}. Output: {output}.")
     return json.loads(output)
 
 
@@ -45,15 +59,16 @@ def get_duration(audio_filename):
     """
     return float(get_ffprobe_entry(audio_filename, "duration"))
 
+
 def get_sample_rate(audio_filename):
     """
         Get sample rate of audio filename in Hz
     """
     return float(get_ffprobe_entry(audio_filename, "sample_rate"))
 
+
 def get_channel_number(audio_filename):
     """
         Get number of channels  of audio filename
     """
     return int(get_ffprobe_entry(audio_filename, "channels"))
-
