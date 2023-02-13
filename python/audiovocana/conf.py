1c44c0bdde5c3de7461520c998ae0e060da94f5c
jimenaRL <jimena@melix.net>
2023-02-13 13:58:26 +0100

    Change STFT default parameters to Paloma ones, add f0 computation params and add stft plot params to conf

--- python/audiovocana/conf.py
+++ python/audiovocana/conf.py
@@ -193,22 +193,21 @@ AUDIOPARAMS = {
 # --------------------------------------------------------------------
 
 # 1st  set paramaters, used in results from April 10th, 2020
-NFFT = 2048*4
-HOPLENGTH = int(NFFT/16)
+# NFFT = 2048*4
+# HOPLENGTH = int(NFFT/16)
+# CENTER_WINDOWS = True
+# NMELS = 128
+# MIN_STFT_LENGTH = 9
+
+# 2nd  set paramaters, used by Paloma, more close to avisoft software ones
+NFFT = 200
+HOPLENGTH = 8
 CENTER_WINDOWS = True
-NMELS = 128
+NMELS = 32
 MIN_STFT_LENGTH = 9
 
 
-# 2nd set paramaters
-# NFFT = 2048
-# HOPLENGTH = int(NFFT/4)
-# CENTER_WINDOWS = False
-# NMELS = 64
-# MIN_STFT_LENGTH = 5
-
 # --------------------------------------------------------------------
-
 
 # STFT PARAMS
 NFFT = NFFT
@@ -224,7 +223,9 @@ STFTPARAMS = {
     'win_length': WINLENGTH,
     'center': CENTER_WINDOWS,
     'window': WINDOW,
+    # 'pad_mode': 'reflect',
 }
+
 
 FFTFREQS = librosa.core.fft_frequencies(
     sr=AUDIOPARAMS['sr'],
@@ -296,9 +297,37 @@ ZEROCRPARAMS = {
     'center': CENTER_WINDOWS,
 }
 
+
+# F0 PARAMS
+F0PARAMS = {
+    # (3) tonality thresholding of contour
+    # A high spectral flatness (closer to 1.0) indicates
+    # the spectrum is similar to white noise
+    #     f0_max_th = f0_max[flateness < tonality_threshold]
+    'tonality_threshold': 0.75,
+    # (4) contour smoothing by spline interpolation
+    'smoothness': 0.5,
+    # take only central part as borders are noisy
+    'ignore_border': 0.15
+}
+
+# PLOT STFT PARAMS
+
+STFTPLOTPARAMS = {
+    # for x axis settings
+    'sr': SR,
+    'hop_length': HOPLENGTH,
+    'x_axis': 'ms',
+    # for y axis settings
+    'fmax': FMAX,
+    'y_axis': 'linear',
+    'cmap': 'magma'  # 'gray', 'bone', 'magma'
+}
+
+
 # SET MIN AUDIO LENGTH ALLOWED
 if CENTER_WINDOWS:
-    MIN_WAVEFORM_LENGTH = int(WINLENGTH/2) + (MIN_STFT_LENGTH - 1)
+    MIN_WAVEFORM_LENGTH = int(WINLENGTH / 2) + (MIN_STFT_LENGTH - 1)
 else:
     MIN_WAVEFORM_LENGTH = WINLENGTH + (MIN_STFT_LENGTH - 1) * HOPLENGTH
 MIN_AUDIO_LENGHT_MS = MIN_WAVEFORM_LENGTH * 1e3 / SR
@@ -307,12 +336,13 @@ MIN_AUDIO_LENGHT_MS = MIN_WAVEFORM_LENGTH * 1e3 / SR
 print("~~~~~~ AUDIOVOCANA SETTINGS ~~~~~~")
 print(f"AUDIOPARAMS \n {AUDIOPARAMS}")
 print(f"STFTPARAMS \n {STFTPARAMS}")
-print(f"SPECTRALCENTROIDPARAMS \n {SPECTRALCENTROIDPARAMS}")
-print(f"SPECTRALBANDWIDTHPARAMS \n {SPECTRALBANDWIDTHPARAMS}")
-print(f"SPECTRALFLATNESSPARAMS \n {SPECTRALFLATNESSPARAMS}")
-print(f"ZEROCRPARAMS \n {ZEROCRPARAMS}")
-print(f"MELPARAMS \n {MELPARAMS}")
-print(f"MFCCPAMARS \n {MFCCPAMARS}")
+print(f"F0PARAMS \n {F0PARAMS}")
+# print(f"SPECTRALCENTROIDPARAMS \n {SPECTRALCENTROIDPARAMS}")
+# print(f"SPECTRALBANDWIDTHPARAMS \n {SPECTRALBANDWIDTHPARAMS}")
+# print(f"SPECTRALFLATNESSPARAMS \n {SPECTRALFLATNESSPARAMS}")
+# print(f"ZEROCRPARAMS \n {ZEROCRPARAMS}")
+# print(f"MELPARAMS \n {MELPARAMS}")
+# print(f"MFCCPAMARS \n {MFCCPAMARS}")
 print(f"mel fiterbank shape = {MELFB.shape}")
 print(f"Minimun waveform length accepted is {MIN_WAVEFORM_LENGTH} PCM points.")
 print(f"Minimun audio duration accepted is {MIN_AUDIO_LENGHT_MS} miliseconds.")
