42ccc9f852c6df7c7d82cceab5533028b5bd9970
jimenaRL <jimena@melix.net>
2023-02-13 13:56:23 +0100

    split plot from f0 computtaion

--- python/audiovocana/f0.py
+++ python/audiovocana/f0.py
@@ -16,24 +16,11 @@ from audiovocana.conf import (
     WINLENGTH,
     NFFT,
     NBFFTBINS,
-    STFTPARAMS
+    STFTPARAMS,
+    STFTPLOTPARAMS
 )
 print(f"f0 estimation STFT frequency resolution = {FMAX / NBFFTBINS} Hz.")
 print(f"STFT time resolution = {1e3 * NFFT / SR} ms.")
-
-STFTPLOTPARAMS = {
-    # for x axis settings
-    'sr': SR,
-    'hop_length': HOPLENGTH,
-    'x_axis': 'time',
-    # for y axis settings
-    'fmax': FMAX,
-    'y_axis': 'linear',
-    # color
-    # 'cmap': 'gray', # 'gray', 'PuBu_r', 'RdBu', 'bone'
-    'vmin': -80,  # -50, -25
-    'vmax': 0
-}
 
 
 def find_outliers(data, n_quintiles):
@@ -47,8 +34,8 @@ def find_outliers(data, n_quintiles):
     low_lim = qs[0] - 1.5 * IQR / FMAX
     up_lim = qs[-1] + 1.5 * IQR
 
-    A = low_lim<=data
-    B = data<=up_lim
+    A = low_lim <= data
+    B = data <= up_lim
 
     res = (A * B)
     return res
@@ -66,13 +53,9 @@ def f0_estimation(
     audio_path,
     t0,
     duration,
-    tonality_threshold=0.85,
-    smoothness=0.01,
-    ignore_border=0.15,
-    title='',
-    image_path=None,
-    show=False):
-
+    tonality_threshold,
+    smoothness,
+    ignore_border):
 
     y, sr = ffmpeg.load_audio_file(
         audio_filename=audio_path,
@@ -81,22 +64,35 @@ def f0_estimation(
 
     y = y.flatten()
 
-    # amplitud spectrogram
-    D = np.abs(librosa.stft(y, **STFTPARAMS))
+    S = librosa.stft(y, **STFTPARAMS)
+
+    return f0_estimation_from_spect(
+        S,
+        tonality_threshold,
+        smoothness,
+        ignore_border
+    )
+
+
+def f0_estimation_from_spect(
+    S,
+    tonality_threshold,
+    smoothness,
+    ignore_border,
+    plot=False,
+    title=None,
+    image_path=None
+    ):
+
+    spectrogram = S
+
+    # (1) amplitud spectrogram
+    D = np.abs(S)
 
     # compute time coordinates
     x_lim = frames_to_time(
         np.arange(D.shape[1] + 1), sr=SR, hop_length=HOPLENGTH)[-1]
     x = np.linspace(0, x_lim, D.shape[1])
-
-    # (1) plot STFT
-    if show:
-        fig, ax = plt.subplots(
-            nrows=2, sharex=False, sharey=False, figsize=(16, 14))
-        img = librosa.display.specshow(
-            librosa.amplitude_to_db(D, ref=np.max), ax=ax[1], **STFTPLOTPARAMS)
-        _title = f'duration = {10**3 * duration:3.0f} ms \n {title}'
-        ax[0].set(title=_title)
 
     # (2) compute contour = max f0 over STFT
 
@@ -105,26 +101,18 @@ def f0_estimation(
         D, mode='affinity', metric='cosine', sparse=True)
     D = librosa.decompose.nn_filter(D, rec=rec, aggregate=np.average)
 
-    if show:
-        img = librosa.display.specshow(
-            librosa.amplitude_to_db(D, ref=np.max), ax=ax[0], **STFTPLOTPARAMS)
-
     # ignore bass frequencies on powerspectrogram
     f_cut = 16
-    S = D[f_cut:, :]
-    Spower = S**2
+    S_cut = D[f_cut:, :]
+    Spower = S_cut**2
     freqs = np.linspace(0, FMAX, D.shape[0])[f_cut:]
 
     # prepare array for peak max f0 at each time bin
-    f0 = np.zeros(S.shape[1])
+    f0 = np.zeros(S_cut.shape[1])
 
-    A = np.argmax(S, axis=0)
+    A = np.argmax(S_cut, axis=0)
     for t, f in enumerate(A):
         f0[t] = freqs[f]
-
-    if show:
-        ax[0].plot(x, f0, color='r', marker='^', linewidth=0, label='max')
-        ax[0].legend(loc='upper right')
 
     # (3) tonality thresholding of contour
     # A high spectral flatness (closer to 1.0) indicates
@@ -134,12 +122,7 @@ def f0_estimation(
     f0_tonality = f0[flateness < tonality_threshold]
 
     x1 = x[flateness < tonality_threshold]
-    S1 = S[:, flateness < tonality_threshold]
-
-    if show:
-        ax[0].plot(
-            x1, f0_tonality, color='g', marker='.', linewidth=0, label='f0_tonality')
-        ax[0].legend(loc='upper right')
+    S1 = S_cut[:, flateness < tonality_threshold]
 
     # (4) outliers removing from contour
     outliers = find_outliers(np.abs(np.diff(f0_tonality)), n_quintiles=20)
@@ -149,7 +132,8 @@ def f0_estimation(
     S2 = S1[:, 2:][:, outliers]
 
     # (4) contour smoothing by spline interpolation
-    tck = sp.interpolate.splrep(x2, f0_tonality_outliers / FMAX, k=3, s=smoothness)
+    tck = sp.interpolate.splrep(
+        x2, f0_tonality_outliers / FMAX, k=3, s=smoothness)
     f0_smooth = FMAX * sp.interpolate.splev(x, tck, der=0)
     # take only central part as borders are noisy
     t = ignore_border
@@ -171,35 +155,65 @@ def f0_estimation(
     rms = librosa.feature.rms(S=S2, frame_length=168, hop_length=42).flatten()
     mean_rms = np.mean(rms)
 
-    if show:
-        ax[0].plot(
+    if plot:
+        plot_f0(
+            spectrogram, x, f0, x1, f0_tonality,
             x2, f0_tonality_outliers,
-            color='b', marker='x', linewidth=0, label='f0_tonality_outliers')
-        ax[0].legend(loc='upper right')
+            f0_smooth, mnddy, title, image_path
+        )
 
-        ax[0].plot(
-            x, f0_smooth,
-            color='m', marker='x', linewidth=0, label='f0_smooth')
-        ax[0].legend(loc='upper right')
+    return [
+        min_f0,
+        max_f0,
+        mean_f0,
+        mnddy,
+        mean_rms
+    ]
 
-        _title = ''
-        _title += f'\n mean normalized 2dn derivative = {mnddy* 1e-3}'
-        _title += f'\n mean rms = {mean_rms}'
-        _title += f'\n max f0 = {max_f0}'
-        _title += f'\n mean f0 = {mean_f0}'
-        _title += f'\n min f0 = {min_f0}'
-        plt.title(_title)
-        plt.tight_layout()
 
-        fig.colorbar(img, ax=ax, format="%+2.f dB")
+def plot_f0(
+    spectrogram, x, f0, x1, f0_tonality, x2, f0_tonality_outliers,
+    f0_smooth, mnddy, title='', image_path=None):
 
-        if image_path:
-            fig.savefig(image_path)
+    # (1) plot STFT
+    fig, ax = plt.subplots(figsize=(12, 6))
+    img = librosa.display.specshow(
+        librosa.amplitude_to_db(spectrogram),
+        ax=ax,
+        **STFTPLOTPARAMS)
+    ax.set(title=title)
 
-    return {
-        'min_f0': min_f0,
-        'max_f0': max_f0,
-        'mean_f0': mean_f0,
-        'mean_2dn_derivative': mnddy,
-        'mean_rms': mean_rms
-    }
+    # (2) compute contour = max f0 over STFT
+
+    ax.plot(x, f0, color='r', marker='^', linewidth=0, label='max')
+    ax.legend(loc='upper right')
+
+    # (3) tonality thresholding of contour
+
+    ax.plot(
+        x1, f0_tonality,
+        color='g', marker='.', linewidth=0, label='f0_tonality')
+    ax.legend(loc='upper right')
+
+    # (4) outliers removing from contour
+    # (4) contour smoothing by spline interpolation
+
+    # (5) compute mean normalize 2nd derivative from normalized contour
+
+    ax.plot(
+        x2, f0_tonality_outliers,
+        color='b', marker='x', linewidth=0, label='f0_tonality_outliers')
+    ax.legend(loc='upper right')
+
+    ax.plot(
+        x, f0_smooth,
+        color='m', marker='x', linewidth=0, label='f0_smooth')
+    ax.legend(loc='upper right')
+
+    plt.title(title)
+    plt.tight_layout()
+
+    fig.colorbar(img, ax=ax, format="%+2.f dB")
+
+    if image_path:
+        fig.savefig(image_path)
